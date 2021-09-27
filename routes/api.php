7ae5c002a1dfda91226d2e5f1d1ddd41dacb1ddf
oscardg77 <82647316+oscardg77@users.noreply.github.com>
2021-09-27 08:56:28 +0200

    actualizacion de rutas

--- routes/api.php
+++ routes/api.php
@@ -2,40 +2,51 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\SupermercadoController;
-use App\Http\Controllers\AuthSupermercadoController;
+use App\http\Controllers\UserController;
+use App\Http\Controllers\AuthUserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthCategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\AuthOrderController;
 
 
-Route::get('user',[SupermercadoController::class, 'index']);
-Route::get('/user/{id}', [SupermercadoController::class, 'show']);
-Route::post('/user', [SupermercadoController::class, 'store']);
-Route::put('/user/{id}', [SupermercadoController::class, 'update']);
-Route::delete('/user/{id}', [SupermercadoController::class, 'destroy']);
+Route::get('user',[UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::post('/user', [UserController::class, 'store']);
+Route::put('/user/{id}', [UserController::class, 'update']);
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
+Route::post('/user',[UserController::class, 'AuthUserController']);
 
-Route::get('order',[SupermercadoController::class, 'order']);
-Route::get('/order/{id}', [SupermercadoController::class, 'show']);
-Route::post('/order', [SupermercadoController::class, 'store']);
-Route::put('/order/{id}', [SupermercadoController::class, 'update']);
-Route::delete('/order/{id}', [SupermercadoController::class, 'destroy']);
-
-
-Route::get('product',[SupermercadoController::class, 'product']);
-Route::get('/product/{id}', [SupermercadoController::class, 'show']);
-Route::post('/product', [SupermercadoController::class, 'store']);
-Route::put('/product/{id}', [SupermercadoController::class, 'update']);
-Route::delete('/product/{id}', [SupermercadoController::class, 'destroy']);
+Route::get('order',[OrderController::class, 'order']);
+Route::get('/order/{id}', [OrderController::class, 'show']);
+Route::post('/order', [OrderController::class, 'store']);
+Route::put('/order/{id}', [OrderController::class, 'update']);
+Route::delete('/order/{id}', [OrderController::class, 'destroy']);
+Route::post('/Order',[OrderController::class, 'AuthOrderController']);
 
 
-Route::get('category',[SupermercadoController::class, 'category']);
-Route::get('/category/{id}', [SupermercadoController::class, 'show']);
-Route::post('/category', [SupermercadoController::class, 'store']);
-Route::put('/category/{id}', [SupermercadoController::class, 'update']);
-Route::delete('/category/{id}', [SupermercadoController::class, 'destroy']);
+Route::get('product',[ProductController::class, 'product']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::put('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+Route::post('/product',[ProductController::class, 'AuthProductController']);
+
+
+Route::get('category',[CategoryController::class, 'category']);
+Route::get('/category/{id}', [CategoryController::class, 'show']);
+Route::post('/category', [CategoryController::class, 'store']);
+Route::put('/category/{id}', [CategoryController::class, 'update']);
+Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
+Route::post('/category',[CategoryController::class, 'AuthCategoryController']);
 
 
 
-Route::post('/login', [AuthSupermercadoController::class, 'login']);
-Route::post('/register', [AuthSupermercadoController::class, 'register']);
+Route::post('/login', [AuthUserController::class, 'login']);
+Route::post('/register', [AuthUserController::class, 'register']);
+Route::get('/logout', [AuthUserController::class, 'logout']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
